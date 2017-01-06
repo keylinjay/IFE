1fde91b3a456d3ffc2ca17e353847e919f098547
keylinjay <k547531366@hotmail.com>
2017-01-06 17:31:38 +0800

    update readme.md, from asus

---
+++ layoutlink.ps1
@@ -0,0 +1,21 @@
+#windows ps script 
+#layout to readme.md 
+#author:linjay
+#createdate:2016-12-11
+#update:2016-12-11
+
+(test-path README.MD) -and (rm README.MD)
+#def locale pre path
+$lppath=$(pwd).path.replace("\","/")
+#def remote pre path
+$rppath='https://keylinjay.github.io/IFE-baidu-front-end-lessons-practice'
+
+ls .\2016_spring -r |?{$_.name -like '*.html'}| foreach-object{$path=$_.versioninfo.filename.replace("\","/");
+    $link=$path.replace($lppath,$rppath);
+    $linkname=$link.replace("/index.html","");
+    $linkname=$linkname.replace($rppath,"");
+    echo "[$linkname]($link)">>test;
+    echo "">> test;
+}
+get-content test | out-file README.MD -encoding utf8
+rm test
