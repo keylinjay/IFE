500dddef8d1b2ebc7523b327cc02d423964b0b6d
keylinjay <k547531366@hotmail.com>
2017-06-30 09:31:45 +0800

    更新了生成链接的脚本

--- layoutlink.ps1
+++ layoutlink.ps1
@@ -10,13 +10,13 @@ $lppath=$(pwd).path.replace("\","/")
 #def remote pre path
 $rppath='https://keylinjay.github.io/IFE-baidu-front-end-lessons-practice'
 
-ls .\2016_spring -r |?{$_.name -like '*.html'}| foreach-object{$path=$_.versioninfo.filename.replace("\","/");
-    $link=$path.replace($lppath,$rppath);
-    $linkname=$link.replace("/index.html","");
-    $linkname=$linkname.replace($rppath,"");
-    echo "[$linkname]($link)">>test;
-    echo "">> test;
-}
+# ls .\2016_spring -r |?{$_.name -like '*.html'}| foreach-object{$path=$_.versioninfo.filename.replace("\","/");
+#     $link=$path.replace($lppath,$rppath);
+#     $linkname=$link.replace("/index.html","");
+#     $linkname=$linkname.replace($rppath,"");
+#     echo "[$linkname]($link)">>test;
+#     echo "">> test;
+# }
 ls .\2017_spring -r |?{$_.name -like '*.html'}| foreach-object{$path=$_.versioninfo.filename.replace("\","/");
     $link=$path.replace($lppath,$rppath);
     $linkname=$link.replace("/index.html","");
