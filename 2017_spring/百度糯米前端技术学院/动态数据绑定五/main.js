c2d3fdc98f159c554b37e2735b268d25a6a7ec1b
keylinjay <k547531366@hotmail.com>
2018-01-20 10:13:16 +0800

    beauty code

--- 2017_spring/百度糯米前端技术学院/动态数据绑定五/main.js
+++ 2017_spring/百度糯米前端技术学院/动态数据绑定五/main.js
@@ -1,37 +1,39 @@
-
 //定义事件中心；
-function Emiter (){
+function Emiter() {
 	var listener = [];
-	function add (key, fn){
-		if(listener.indexOf(key) === -1){
+
+	function add(key, fn) {
+		if (listener.indexOf(key) === -1) {
 			listener.push(key);
-			listener[key]=[];
+			listener[key] = [];
 			listener[key].push(fn);
 		}
-		if(listener[key].indexOf(fn) === -1){
+		if (listener[key].indexOf(fn) === -1) {
 			listener[key].push(fn);
 		}
 	}
-	function trigger (key, args, fn){
-		if(listener.indexOf(key) !== -1){
-			if(fn){
-				if(listener[key].indexOf(fn) !== -1){
-					fn.apply(fn,args);
+
+	function trigger(key, args, fn) {
+		if (listener.indexOf(key) !== -1) {
+			if (fn) {
+				if (listener[key].indexOf(fn) !== -1) {
+					fn.apply(fn, args);
 				}
-			}else{
-				listener[key].forEach(function(fni){
-					fni.apply(fni,args);
+			} else {
+				listener[key].forEach(function (fni) {
+					fni.apply(fni, args);
 				});
-			}		
+			}
 		}
 	}
-	function off(key, fn){
-		if (listener.indexOf(key) !== -1){
-			if (fn){
-				if (listener[key].indexOf(fn) !== -1){
+
+	function off(key, fn) {
+		if (listener.indexOf(key) !== -1) {
+			if (fn) {
+				if (listener[key].indexOf(fn) !== -1) {
 					listener[key].splice(listener[key].indexOf(fn), 1);
 				}
-			}else{
+			} else {
 				listener[key] = [];
 			}
 		}
@@ -44,100 +46,114 @@ function Emiter (){
 	}
 }
 //定义分割字符串的函数
-function textToArray(text){
+function textToArray(text) {
 	var arrOfText = [];
 	var length = text.length;
 	arrOfText = text.split(/{(?={)|}(?=})/);
-	arrOfText = arrOfText.map(function(str){
-		if (str[0] === '{'){
+	arrOfText = arrOfText.map(function (str) {
+		if (str[0] === '{') {
 			return {
-					tag: true,
-					value: str.slice(1)
-					};
+				tag: true,
+				value: str.slice(1)
+			};
 		}
-		if (str[0] === '}'){
-			return {value: str.slice(1)};
+		if (str[0] === '}') {
+			return {
+				value: str.slice(1)
+			};
 		}
-		if(str){
-			return {value: str};
+		if (str) {
+			return {
+				value: str
+			};
 		}
 	});
 	return arrOfText;
 }
-function parseDom(el, obj, emiter){
+
+function parseDom(el, obj, emiter) {
 	var reg = /{{[^{^}]*}}/;
 	parse(el);
-	function parse (el){
-		if(el.nodeType === 1){
-			if(el.dataset.type === 'event'){
-				parseEvent(el);	
+
+	function parse(el) {
+		if (el.nodeType === 1) {
+			if (el.dataset.type === 'event') {
+				parseEvent(el);
 			}
 		}
-		if(el.nodeType === 3){
+		if (el.nodeType === 3) {
 			parseData(el);
 		}
-		if(el.childNodes.length !== 0){
-			[].forEach.call(el.childNodes, function(node){
+		if (el.childNodes.length !== 0) {
+			[].forEach.call(el.childNodes, function (node) {
 				parse(node);
 			});
 		}
 	}
-	function parseData(el){
-		if (!el.nodeValue){return};
-		if(reg.test(el.nodeValue)){
+
+	function parseData(el) {
+		if (!el.nodeValue) {
+			return
+		};
+		if (reg.test(el.nodeValue)) {
 			el._text_ = textToArray(el.nodeValue);
 			binding(el, obj, emiter)
 		}
 	}
-	function parseEvent(el){
+
+	function parseEvent(el) {
 		var data = el.dataset,
 			eventName = data.event,
 			exp = data.val;
-		el.addEventListener(eventName, function(e){
+		el.addEventListener(eventName, function (e) {
 			setval(exp, el.value);
 		}, false);
 	}
-	function setval(exp, val){
+
+	function setval(exp, val) {
 		var arkey = exp.split('.');
 		iter(arkey, obj);
-		function iter(arkey, obj){
-			if(arkey.length === 1){
+
+		function iter(arkey, obj) {
+			if (arkey.length === 1) {
 				obj[arkey[0]] = val;
-			}else{
+			} else {
 				iter(arkey.slice(1), obj[arkey[0]]);
 			}
 		}
 	}
 }
-function binding (el, obj, emiter){
+
+function binding(el, obj, emiter) {
 	var subs = el._text_;
-	var vals = subs.map(function(obj){
-		if(obj.tag){
+	var vals = subs.map(function (obj) {
+		if (obj.tag) {
 			emiter.on('top.' + obj.value, update);
 			return getval(obj.value);
 		}
 		return obj.value;
 	});
-	
-	function getval(exp){
+
+	function getval(exp) {
 		var arkey = exp.split('.');
 		return iter(arkey, obj);
-		function iter(arkey, obj){
-			if(arkey.length !== 1){
+
+		function iter(arkey, obj) {
+			if (arkey.length !== 1) {
 				return iter(arkey.slice(1), obj[arkey[0]]);
 			}
 			return obj[arkey[0]];
 		}
 	}
 
-	function update(){
-		vals = subs.map(function(obj){
-				if(obj.tag){
-					emiter.on('top.' + obj.value, update);
-					return getval(obj.value);
-				}
-				return obj.value;
-			});
+	function update() {
+		vals = subs.map(function (obj) {
+			if (obj.tag) {
+				emiter.on('top.' + obj.value, update);
+				return getval(obj.value);
+			}
+			return obj.value;
+		});
 		console.log('我要更新了');
 		el.nodeValue = vals.join('');
 	}
@@ -145,14 +161,14 @@ function binding (el, obj, emiter){
 }
 
 //定义Vue
-var Vue = function (obj){
+var Vue = function (obj) {
 	this.data = obj.data;
 	this.el = document.querySelectorAll(obj.el)[0];
 	this.binds = [];
 	this.emiter = Emiter();
 	console.log(this.emiter);
 	this.nodes = document.querySelectorAll(obj.el);
-	this.htmls = [].map.call(this.nodes, function(node){
+	this.htmls = [].map.call(this.nodes, function (node) {
 		return node.innerHTML;
 	});
 	parseDom(this.el, this.data, this.emiter);
@@ -160,22 +176,23 @@ var Vue = function (obj){
 }
 var proto = Vue.prototype;
 // 定义对象转换的方法。
-proto.convertf = function (obj){
+proto.convertf = function (obj) {
 	var self = this;
-	function convertf (obj) {
-		function subConvertf (key, val){	
-			Object.defineProperty(obj, key, {	
+
+	function convertf(obj) {
+		function subConvertf(key, val) {
+			Object.defineProperty(obj, key, {
 				enumerable: true,
 				configurable: true,
-				get: function (){
+				get: function () {
 					// console.log('你访问了' + key + ":" +val);
 					return val;
 				},
-				set: function (newval){
+				set: function (newval) {
 					// console.log('你设置了' + key);
 					console.log('新的' + key + ' = ' + newval);
 
-					if (typeof newval === 'object'){
+					if (typeof newval === 'object') {
 						convertf(newval);
 					}
 					if (newval === val) return;
@@ -187,11 +204,11 @@ proto.convertf = function (obj){
 			});
 		}
 
-		for (var key in obj){
-			if (obj.hasOwnProperty(key) && key !== '_parent_path_'){
-				if (typeof obj[key] === "object"){
-					obj[key]._parent_path_ = (function(){
-						if(!obj._parent_path_){
+		for (var key in obj) {
+			if (obj.hasOwnProperty(key) && key !== '_parent_path_') {
+				if (typeof obj[key] === "object") {
+					obj[key]._parent_path_ = (function () {
+						if (!obj._parent_path_) {
 							obj._parent_path_ = 'top';
 						}
 						var newpath = obj._parent_path_ + '.' + key;
@@ -203,28 +220,30 @@ proto.convertf = function (obj){
 				subConvertf(key, obj[key]);
 			}
 		}
-		
+
 		return obj;
 	}
 
 	convertf(obj);
 }
 // 定义添加监听事件的方法；
-proto.$watch = function (key, fn){
+proto.$watch = function (key, fn) {
 	console.log(this);
-	var key = 'top'? key : 'top.' + key;
+	var key = 'top' ? key : 'top.' + key;
 	this.emiter.on(key, fn);
 }
-proto.$trigger = function (key, args, fn){
+proto.$trigger = function (key, args, fn) {
 	// console.log(this);
 	var emiter = this.emiter;
 	// console.log(emiter);
 	bubble(key);
-	function bubble (key){
+
+	function bubble(key) {
 		var arkey = key.split('.');
 		iter(arkey);
-		function iter(arkey){
-			if(arkey.length !== 0){
+
+		function iter(arkey) {
+			if (arkey.length !== 0) {
 				var newkey = arkey.join(".");
 				// console.log('newkey is:' + newkey);
 				emiter.trigger(newkey, args, fn);
@@ -245,7 +264,6 @@ var app = new Vue({
 			age: 25,
 			city: 'beijing'
 		},
-		
+
 	}
 });
-
--- 2017_spring/耀耀学院/任务一/main.js
+++ 2017_spring/耀耀学院/任务一/main.js
@@ -1,51 +1,51 @@
 // 定义元素选择函数
-function $q (str, env){
+function $q(str, env) {
     var parentEl = env || window.document;
     return parentEl.querySelectorAll(str);
 }
-$q.hasClass = (el, className)=>{
+$q.hasClass = (el, className) => {
     // 正则表达式匹配一个单词及后面的空格。因为是用新建正则对象的方法定义，所以用两个斜杠，第一个表示转义。
     var RegClass = new RegExp('\\s*' + className + '\\s*', 'g');
-    
+
     // 搜索结果里面是否含有指定的class
-    if (RegClass.test(el.className)){
+    if (RegClass.test(el.className)) {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
-$q.addClass = (el, className)=>{
-    if ($q.hasClass(el, className)){
+$q.addClass = (el, className) => {
+    if ($q.hasClass(el, className)) {
         return;
-    }else{
+    } else {
         el.className = el.className + " " + className;
     }
 }
-$q.removeClass = (el, className)=>{
+$q.removeClass = (el, className) => {
     var RegClass = new RegExp('\\s*' + className + '\\s*', 'g');
-    if ($q.hasClass(el, className)){
+    if ($q.hasClass(el, className)) {
         el.className = el.className.replace(RegClass, " ");
     }
 }
-$q.toggleClass = (el, className)=>{
-    if ($q.hasClass(el, className)){
+$q.toggleClass = (el, className) => {
+    if ($q.hasClass(el, className)) {
         $q.removeClass(el, className);
-    }else{
+    } else {
         $q.addClass(el, className);
     }
 }
 // 定义事件代理函数
-function eventProx (el, className, eventName, handleFn, boolean){
-    var iscapture = boolean ||  false;
+function eventProx(el, className, eventName, handleFn, boolean) {
+    var iscapture = boolean || false;
     //判断浏览器是否支持addEventListener,并采取不同的处理流程
-    if (window.addEventListener){
-        el.addEventListener(eventName, (event)=>{
+    if (window.addEventListener) {
+        el.addEventListener(eventName, (event) => {
             // 获取事件对象
             var event = event || window.event;
             //定义触发事件的元素
             var target = event.target;
             //判断触发事件的元素是否是指定的元素。即它的className中是否含有我们指定的className
-            if ($q.hasClass(target, className)){
+            if ($q.hasClass(target, className)) {
                 handleFn(event);
             }
         }, iscapture);
@@ -73,28 +73,28 @@ dataCenter.push(test2);
 dataCenter.push(test3);
 
 // 根据数据中心的数据为代理元素添加对应的事件
-dataCenter.forEach((obj)=>{
+dataCenter.forEach((obj) => {
     var proxEl = obj.el;
     console.log(proxEl);
     var className = 'btn';
     var eventName = 'click';
-    var handleFn = (event)=>{
+    var handleFn = (event) => {
         var RegConfirm = obj.RegConfirm;
         var parentEl = event.target.parentNode;
         var confirmVal = $q('.confirm-value', parentEl)[0].value;
         var confirmTextEl = $q('.confirm-result', parentEl)[0];
-        if (confirmVal === ''){
+        if (confirmVal === '') {
             confirmTextEl.innerText = '姓名不能为空';
             $q.removeClass(confirmTextEl, 'success');
             $q.addClass(confirmTextEl, 'error');
-        }else if(RegConfirm.test(confirmVal)){
+        } else if (RegConfirm.test(confirmVal)) {
             confirmTextEl.innerText = '名称格式正确';
             $q.removeClass(confirmTextEl, 'error');
             $q.addClass(confirmTextEl, 'success');
-        }else{
+        } else {
             confirmTextEl.innerText = '必填，长度4~16个字符';
         }
-         
+
     };
     eventProx(proxEl, className, eventName, handleFn);
 });
